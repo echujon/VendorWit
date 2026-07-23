879ee60bd66e87647d3d4c521248558a0726c39c
Efrain Chu-Jon <fchujon@hotmail.com>
2026-07-23 16:24:22 -0600

    changes to take to Point of Sale API

--- tag-scanner/sw.js
+++ tag-scanner/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'tag-scanner-v13';
+const CACHE = 'tag-scanner-v14';
 const ASSETS = [
   '/', '/index.html', '/settings/', '/css/style.css', '/js/app.js', '/js/storage.js',
   'https://cdn.jsdelivr.net/npm/tesseract.js@5/dist/tesseract.min.js',
